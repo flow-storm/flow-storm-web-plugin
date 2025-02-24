af66df9f8a82ddbce5182ba04f491c8f5053709d
Juan Monetta <jpmonettas@gmail.com>
2025-02-24 11:48:20 -0300

    Releasing alpha3

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
             [clojure.spec.alpha :as s]))
 
 (def version (or (System/getenv "VERSION")
-                 "1.0.0-alpha2"))
+                 "1.0.0-alpha3"))
 
 (def target-dir "target")
 (def class-dir (str target-dir "/classes"))
